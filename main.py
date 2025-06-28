4ed9d2d4fa012eb35362dce99e32254494caa979
Dang Anh Que <160758822+cinnamoll@users.noreply.github.com>
2025-06-29 00:17:41 +0700

    basic moves?

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ IMAGES = {}
 def loadImg():
     pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
     for p in pieces:
-        IMAGES[p] = pygame.transform.scale(pygame.image.load("Chess-in-python\Chess piece\\" + p + ".png"), (SQR_SIZE, SQR_SIZE))
+        IMAGES[p] = pygame.transform.scale(pygame.image.load("Chess-in-python\\Chess piece\\" + p + ".png"), (SQR_SIZE, SQR_SIZE))
     
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -22,27 +22,53 @@ def main():
     loadImg()
 
     running = True
+    sqSelected = ()
+    playerClick = []
 
     while running:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()
+                col = location[0] // SQR_SIZE
+                row = location[1] // SQR_SIZE 
 
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClick = []
+                else:
+                    sqSelected = (row, col)
+                    playerClick.append(sqSelected)
+                
+                if len(playerClick) == 2:
+                    move = engine.Move(playerClick[0], playerClick[1], state.board)
+                    print(move.getChessNotation())
+                    state.makeMove(move)
+                    sqSelected = ()
+                    playerClick = []
+
+        drawState(screen, state)
         clock.tick(MAX_FPS)
         pygame.display.flip()
 
 
 def drawBoard(screen):
-    colors = [pygame.color("white"), pygame.color("brown")]
+    colors = [pygame.Color("white"), pygame.Color("gray")]
 
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[((r + c) % 2)]
-            pygame.draw.rect(screen, color, pygame.rect(c*SQR_SIZE, r*SQR_SIZE, SQR_SIZE, SQR_SIZE))
+            pygame.draw.rect(screen, color, pygame.Rect(c*SQR_SIZE, r*SQR_SIZE, SQR_SIZE, SQR_SIZE))
             
 
 def drawPieces(screen, board):
-    pass
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c] 
+            if piece != "--":
+               screen.blit(IMAGES[piece], pygame.Rect(c*SQR_SIZE, r*SQR_SIZE, SQR_SIZE, SQR_SIZE)) 
+
 
 def drawState(screen, state):
     drawBoard(screen)
@@ -51,7 +77,3 @@ def drawState(screen, state):
 
 if __name__ == "__main__":
     main()
-
-
-
-#"C:\Code\chess\Chess-in-python\Chess piece\bp.png"
